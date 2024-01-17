d5d20fe2b4446ef5d24d8f88e635a118375afc4f
Duc Phu <vandungqc03@gmail.com>
2024-01-17 16:14:23 +0700

    Change main sys clock frequency to 120 MHz
    - Use HSI as entry clock source for PLL
    - The HSE placement is wrong so we dont use it and it should be PH0 and PH1 pins (HSE) not PC14 and PC15 (LSE)

--- src/main.c
+++ src/main.c
@@ -5,20 +5,56 @@
 extern int main(void);
 
 void GPIOB_Config(void);
+void RCC_ClocksConfig(void);
 void delay(uint32_t time);
 
 int main(void)
 {	
 	GPIOB_Config();
+	RCC_ClocksConfig();
 
 	while(1)
 	{
 		GPIO_Write(12, B, SET);
-		delay(500000);
+		delay(50000);
+		delay(50000);
+		delay(50000);
+		delay(50000);
+		delay(50000);
 		GPIO_Write(12, B, RESET);
-		delay(500000);
+		delay(50000);
+		delay(50000);
+		delay(50000);
+		delay(50000);
+		delay(50000);
 	}
 	return 0;
+}
+
+void RCC_ClocksConfig(void)
+{
+	RCC_SysClkTypeDef clk_init;
+	RCC_APBx_AHB_PreTypeDef prescaler;
+	
+	clk_init.clock 		= PLLCLK;
+	// f(VCO out) = 16 MHz (HSI) * PLLN / PLLM
+	// 100 MHz < f(VCO out) < 432 MHz
+	clk_init.PLLSRC		= HSI;
+	clk_init.PLLN		= 120U;
+	clk_init.PLLM		= 8U;
+	// f(PLL general clock output) = f(VCO clock) / PLLP
+	// This must be <= 168 MHz
+	clk_init.PLLP		= 2U;
+	// f(USB OTG FS, SDIO, RNG clock output) = f(VCO clock) / PLLQ
+	// This must be <= 48 MHz
+	clk_init.PLLQ	 	= 4U;
+	
+	// AHB, APB prescaler
+	prescaler.HPRE 		= 1U;		
+	prescaler.PPRE2		= 2U;		
+	prescaler.PPRE1		= 4U;		
+	
+	RCC_SystemClockInit(&clk_init, &prescaler);
 }
 
 void GPIOB_Config(void)
@@ -39,12 +75,17 @@ void GPIOB_Config(void)
 void delay(uint32_t time)
 {
 	uint32_t temp = time;
+	uint32_t temp_2 = time;
 	while(time > 0)
 	{
 		time -= 1;
 		while(temp > 0) 
 		{
 			temp -= 1;
+			while(temp_2 > 0)
+			{
+				temp_2 -= 1;
+			}
 		}
 	}
 }
