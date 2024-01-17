fcd1ff9264e54db5421efa2d25c7b14a0a392193
Duc Phu <vandungqc03@gmail.com>
2024-01-17 22:33:45 +0700

    Fixed naming error

--- src/main.c
+++ src/main.c
@@ -68,9 +68,9 @@ void TIM_ADC_SampleTime_Config(void)
 {
 	TIM_CounterTypeDef adc_tim;
 	
-	uint16_t sample_time_hz = 1000;
+	uint16_t sample_freq_hz = 1000;
 	// Divide by 2 means the APBx timer clock is multiply by 2 if the APBx prescaler = 1 => x1 else x2
-	adc_tim.PSC = (((STK_Clock * 8) / 2) * 1000000) / sample_time_hz;
+	adc_tim.PSC = (((STK_Clock * 8) / 2) * 1000000) / sample_freq_hz;
 	adc_tim.PSC = adc_tim.PSC - 1;
 	adc_tim.ARR = 500;	// 500ms
 	adc_tim.ARPE = 1;
